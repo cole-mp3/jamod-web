443a7ad4917dadf40907618eed7dbd431db250ae
Jabong <99981800+cole-mp3@users.noreply.github.com>
2025-11-18 09:44:12 -0500

    more operators

--- jamod/src/seals.lua
+++ jamod/src/seals.lua
@@ -77,7 +77,7 @@ SMODS.Seal {
                     "Adds the {C:blue}+chips{} value of the",
                     "current played hand whne scored."
                 },
-                label = "Operator (addition)"
+                label = "Addition"
     },
     calculate = function(self, card, context)
             if context.main_scoring and context.cardarea == G.play or context.repetiton and context.cardarea == G.play then
@@ -118,6 +118,74 @@ SMODS.Seal {
 
 
 }
+SMODS.Seal {
+    key = 'opermul',
+     badge_colour = HEX("1d4fd7"),
+    atlas = "satlas",
+    pos = {x=0, y=0},
+    config = {extra = {Xmult = 2}}
+
+    loc_txt = {
+                name = "Multiplication",
+                text = {
+                    "Multiplies the Mult value of the played hand ",
+                    "by the {C:blue}+chips{} value of the",
+                    "current played hand when scored."
+                },
+                label = "Multiplication"
+    },
+    calculate = function(self, card, context)
+            if context.main_scoring and context.cardarea == G.play or context.repetiton and context.cardarea == G.play then
+                local current_hand_chips = hand_chips
+                local current_hand_mult = hand_mult
+                return {
+                    xmult = hand_chips * hand_mult
+                }
+            end
+    end,
+
+
+}
+SMODS.Seal {
+    key = 'opersq',
+     badge_colour = HEX("1d4fd7"),
+    atlas = "satlas",
+    pos = {x=0, y=0},
+    config = {extra = {mult = 90}},
+    loc_vars = function(self, info_queue, card)
+        return {
+            self.config.extra.mult
+        }
+    end,
+    loc_txt = {
+                name = "Square root",
+                text = {
+                    "This Card gives +#1# mult,",
+                    "and (if possible), is considered ",
+                    "it's {C:attention}square root.{}",
+                    "{C:inactive}but i cant code so for now they're gonna be treated as gold{}"
+                },
+                label = "Sqrt (x)"
+    },
+    calculate = function(self, card, context)
+            if context.main_scoring and context.cardarea == G.play or context.repetiton and context.cardarea == G.play then
+                return {
+                    mult = self.config.extra.mult
+                }
+            end
+            if context.check_enhancement and context.other_card.base.value == "4" or
+            context.check_enhancement and context.other_card.base.value == "9" or
+            context.check_enhancement and context.other_card.base.value == "14" --Aces are ones!
+            then
+            return {
+                m_gold = true,
+                 }
+            end
+    end,
+
+
+}
+
 -- all seal is getting a recode.
 -- just uhh
 -- not now
